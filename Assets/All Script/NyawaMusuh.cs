562f47462b5372d4e61c40e90eb28aed542aae5e
Poltek VR <143823375+NicholasCod@users.noreply.github.com>
2025-05-26 11:58:59 +0700

    etrbaru

---
+++ Assets/All Script/NyawaMusuh.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NyawaMusuh : MonoBehaviour
+{
+    public float nyawaCurrent = 10;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("projectile"))
+        {
+            nyawaCurrent -= 1;
+            MusuhMati();
+        }
+        else if(other.gameObject.CompareTag("projectileLvl2"))
+        {
+            nyawaCurrent -= 2;
+            MusuhMati();
+        }
+    }
+
+    void MusuhMati()
+    {
+        if (nyawaCurrent <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
